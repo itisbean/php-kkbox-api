b0120aeaf88ca5fd1047435e5743be6efd14bd38
doun <iamdonydou@gmail.com>
2020-11-17 21:23:38 +0800

    kkbox open api

---
+++ demo/run.php
@@ -0,0 +1,24 @@
+<?php
+
+use kkboxMusic\Api;
+
+include_once __DIR__.'/../src/Api.php';
+include_once __DIR__.'/../src/Storage.php';
+include_once __DIR__.'/../vendor/autoload.php';
+
+
+$id = '';
+$secret = '';
+$proxy = '';
+
+$api = new Api($id, $secret, $proxy);
+$ret = $api->searchSinger('容祖儿');
+// $ret = $api->getSingerInfo('CkUufnD9LnwAcywq5t');
+// $ret = $api->getSingerAlbums('CkUufnD9LnwAcywq5t');
+// $ret = $api->getSingerTopSongs('CkUufnD9LnwAcywq5t');
+// $ret = $api->getAlbumSongs('1Z6CsH0Xk0A9vdEt85');
+// $ret = $api->getSongs('KpSmZuFEsGgkoPc3lq,4scNX6sjOGRFho93Eq');
+// $ret = $api->getCharts();
+// $ret = $api->getChartSongs('0sctvfDZM6d4BMqBSW');
+// $ret = $api->getSingerCharts('CkUufnD9LnwAcywq5t');
+echo json_encode($ret, JSON_UNESCAPED_UNICODE);
---
+++ src/Api.php
@@ -0,0 +1,346 @@
+<?php
+
+namespace kkboxMusic;
+
+use GuzzleHttp\Client;
+use kkboxMusic\Storage;
+
+class Api {
+
+    protected $_client;
+
+    protected $_clientId;
+    protected $_clientSecret;
+
+    protected $_proxy;
+
+    public function __construct($clientId, $clientSecret, $proxy = '')
+    {
+        $this->_client = new Client();
+        $this->_clientId = $clientId;
+        $this->_clientSecret = $clientSecret;
+        $this->_proxy = $proxy;
+    }
+
+    /**
+     * oauth2 token
+     * @return string
+     */
+    private function _getToken()
+    {
+        $dbName = 'accessToken';
+        $result = Storage::init()->get($dbName, $this->_clientId);
+        // 是否已存在且未过期
+        if ($result && !empty($result['token'])) {
+            if (isset($result['expires']) && $result['expires'] > time()) {
+                return $result['token'];
+            }
+            // refresh token
+            $params = [
+                'grant_type' => 'refresh_token',
+                'refresh_token' => $result['token']
+            ];
+        } else {
+            $params = [
+                'grant_type' => 'client_credentials',
+                'client_id' => $this->_clientId,
+                'client_secret' => $this->_clientSecret
+            ];
+        }
+        
+        $url = 'https://account.kkbox.com/oauth2/token';
+        $option = ['form_params' => $params];
+        if ($this->_proxy) {
+            $option['proxy'] = $this->_proxy;
+        }
+        try {
+            $response = $this->_client->post($url, $option);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $this->_error('get token failed, [' . $response->getStatusCode().']'. $e->getMessage(), false);
+        }
+        $result = $response->getBody()->getContents();
+        $result = json_decode($result, true);
+        $token = $result['access_token'];
+        $expires = $result['expires_in'] + time();
+        Storage::init()->set($dbName, $this->_clientId, ['token' => $token, 'expires' => $expires]);
+        return $token;
+        
+    }
+
+    /**
+     * 搜索
+     * @param string $keyword
+     * @param string $type 可选 track,album,artist,playlist
+     * @param integer $page 页数，默认1
+     * @param integer $pageSize 每页条数，默认15，最大50
+     * @return array
+     */
+    public function search($keyword, $type = '', $page = 1, $pageSize = 15)
+    {
+        $page > 0 || $page = 1;
+        $pageSize > 0 || $pageSize = 15;
+        $url = 'https://api.kkbox.com/v1.1/search';
+        $param = [
+            'q' => $keyword,
+            'type' => $type ?: 'track,album,artist,playlist',
+            'territory' => 'HK', // 可选 HK,JP,MY,SG,TW
+            'offset' => ($page - 1) * $pageSize,
+            'limit' => $pageSize
+        ];
+        $url .= '?'.http_build_query($param);
+
+        $option = ['headers' => ['authorization' => 'Bearer '. $this->_getToken()]];
+        if ($this->_proxy) {
+            $option['proxy'] = $this->_proxy;
+        }
+        try {
+            $response = $this->_client->get($url, $option);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $this->_error('get token failed, [' . $e->getCode().'] '. $e->getMessage());
+        }
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        return $this->_success($data);
+    }
+
+    /**
+     * 搜索歌手
+     * @param string $keyword 歌手名
+     * @return array
+     */
+    public function searchSinger($keyword)
+    {
+        return $this->search($keyword, 'artist', 1, 50);
+    }
+
+    /**
+     * 搜索歌曲
+     * @param string $keyword 歌曲名
+     * @param integer $page 页数，默认1
+     * @param integer $pageSize 每页条数，默认15
+     * @return array
+     */
+    public function searchSongs($keyword, $page = 1, $pageSize = 15) 
+    {
+        return $this->search($keyword, 'track', $page, $pageSize);
+    }
+
+    /**
+     * 获取歌手信息
+     * @param string $singerId 歌手ID
+     * @return array
+     */
+    public function getSingerInfo($singerId)
+    {
+        $url = 'https://api.kkbox.com/v1.1/artists/'.$singerId.'?territory=HK';
+        $option = ['headers' => ['authorization' => 'Bearer '. $this->_getToken()]];
+        if ($this->_proxy) {
+            $option['proxy'] = $this->_proxy;
+        }
+        try {
+            $response = $this->_client->get($url, $option);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $this->_error('get token failed, [' . $e->getCode().'] '. $e->getMessage());
+        }
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        // TODO followers
+        return $this->_success($data);
+    }
+
+    /**
+     * 获取歌手专辑
+     * @param string $singerId 歌手ID
+     * @return array
+     */
+    public function getSingerAlbums($singerId)
+    {
+        $url = 'https://api.kkbox.com/v1.1/artists/'.$singerId.'/albums?territory=HK&limit=500'; // limit最大500
+        $option = ['headers' => ['authorization' => 'Bearer '. $this->_getToken()]];
+        if ($this->_proxy) {
+            $option['proxy'] = $this->_proxy;
+        }
+        try {
+            $response = $this->_client->get($url, $option);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $this->_error('get token failed, [' . $e->getCode().'] '. $e->getMessage());
+        }
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        unset($data['paging']);
+        foreach ($data['data'] as &$val) {
+            unset($val['artist']);
+        }
+        return $this->_success($data);
+    }
+
+    /**
+     * 获取歌手热门歌曲
+     * @param string $singerId 歌手ID
+     * @return array
+     */
+    public function getSingerTopSongs($singerId)
+    {
+        $url = 'https://api.kkbox.com/v1.1/artists/'.$singerId.'/top-tracks?territory=HK&limit=500'; // limit最大500
+        $option = ['headers' => ['authorization' => 'Bearer '. $this->_getToken()]];
+        if ($this->_proxy) {
+            $option['proxy'] = $this->_proxy;
+        }
+        try {
+            $response = $this->_client->get($url, $option);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $this->_error('get token failed, [' . $e->getCode().'] '. $e->getMessage());
+        }
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        unset($data['paging']);
+        return $this->_success($data);
+    }
+
+    /**
+     * 获取专辑歌曲
+     * @param string $albumId 专辑ID
+     * @return array
+     */
+    public function getAlbumSongs($albumId)
+    {
+        $url = 'https://api.kkbox.com/v1.1/albums/'.$albumId.'/tracks?territory=HK&limit=500'; // limit最大500
+        $option = ['headers' => ['authorization' => 'Bearer '. $this->_getToken()]];
+        if ($this->_proxy) {
+            $option['proxy'] = $this->_proxy;
+        }
+        try {
+            $response = $this->_client->get($url, $option);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $this->_error('get token failed, [' . $e->getCode().'] '. $e->getMessage());
+        }
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        unset($data['paging']);
+        return $this->_success($data);
+    }
+
+    /**
+     * 获取歌曲信息
+     * @param mixed $songIds 歌曲ID
+     * @return array
+     */
+    public function getSongs($songIds)
+    {
+        if (is_array($songIds)) {
+            $songIds = implode(',', $songIds);
+        }
+        $url = 'https://api.kkbox.com/v1.1/tracks?ids='.$songIds.'&territory=HK';
+        $option = ['headers' => ['authorization' => 'Bearer '. $this->_getToken()]];
+        if ($this->_proxy) {
+            $option['proxy'] = $this->_proxy;
+        }
+        try {
+            $response = $this->_client->get($url, $option);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $this->_error('get token failed, [' . $e->getCode().'] '. $e->getMessage());
+        }
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        isset($data['data']) && $data = $data['data'];
+        return $this->_success($data);
+    }
+
+    /**
+     * 获取榜单列表
+     * @return array
+     */
+    public function getCharts()
+    {
+        $url = 'https://api.kkbox.com/v1.1/charts?territory=HK';
+        $option = ['headers' => ['authorization' => 'Bearer '. $this->_getToken()]];
+        if ($this->_proxy) {
+            $option['proxy'] = $this->_proxy;
+        }
+        try {
+            $response = $this->_client->get($url, $option);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $this->_error('get token failed, [' . $e->getCode().'] '. $e->getMessage());
+        }
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        unset($data['paging']);
+        return $this->_success($data);
+    }
+
+    /**
+     * 获取榜单歌曲
+     * @param string $chartId 榜单ID
+     * @return array
+     */
+    public function getChartSongs($chartId)
+    {
+        $url = 'https://api.kkbox.com/v1.1/charts/'.$chartId.'/tracks?territory=HK&limit=500';
+        $option = ['headers' => ['authorization' => 'Bearer '. $this->_getToken()]];
+        if ($this->_proxy) {
+            $option['proxy'] = $this->_proxy;
+        }
+        try {
+            $response = $this->_client->get($url, $option);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $this->_error('get token failed, [' . $e->getCode().'] '. $e->getMessage());
+        }
+        $result = $response->getBody()->getContents();
+        $data = json_decode($result, true);
+        unset($data['paging']);
+        return $this->_success($data);
+        
+    }
+
+    /**
+     * 获取歌手所在榜单
+     * @param string $singerIds 歌手ID
+     * @return array
+     */
+    public function getSingerCharts($singerIds)
+    {
+        if (!is_array($singerIds)) {
+            $singerIds = explode(',', $singerIds);
+        }
+        $singerIds = array_flip($singerIds);
+        $charts = $this->getCharts()['data'];
+        if (!$charts) {
+            return $this->_error('no charts found.');
+        }
+        $data = [];
+        foreach ($charts['data'] as $chart) {
+            $list = $this->getChartSongs($chart['id'])['data'];
+            if (!$list) {
+                continue;
+            }
+            $songs = [];
+            foreach ($list['data'] as $key => $song) {
+                $singer = $song['album']['artist'];
+                if (isset($singerIds[$singer['id']])) {
+                    $song['rank'] = $key + 1;
+                    $songs[] = $song;
+                }
+            }
+            if ($songs) {
+                $data[] = [
+                    'chart' => $chart,
+                    'songs' => $songs
+                ];
+            }
+        }
+        return $this->_success($data);
+    }
+
+
+    private function _success($data = [])
+    {
+        return ['ret' => true, 'data' => $data, 'msg' => ''];
+    }
+
+    private function _error($msg = '')
+    {
+        return ['ret' => false, 'data' => null, 'msg' => $msg];
+    }
+
+
+}
